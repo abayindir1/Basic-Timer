300d470889353d20c404c48ffa5870dd20b0d167
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-05-08 00:44:15 -0500

    functions are done

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 $(document).ready(function(){
-var audio = new Audio("raven.mp3")
-    $(".btn").click(function(minutes){
+    var audio = new Audio("raven.mp3");
+    $(".btn").click(function(duration){
         var duration = $("#timer-form").val()
         console.log(duration)
-        if(duration <= 0){
+        if(duration <= 0 || duration === null){
             alert("Enter a bigger number")
         }else{
-            minutes = duration -1
+            minutes = duration - 1
             seconds = 60
-            setInterval(function() {
+            console.log(minutes)
+           var timer = setInterval(function() {
                 seconds--
-                if(seconds === 0){
+                if(seconds === -1){
                     minutes--
                     seconds = 60
                 }else if(seconds === 0 && minutes ===0){
-                    clearInterval()
+                    clearInterval(timer)
                     minutes = 0
                     seconds = 0
                     audio.play()
                 }
                 console.log(minutes + ":" + seconds)
             },1000)
+
         }
     })
     
